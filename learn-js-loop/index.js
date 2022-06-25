0d0fd013b394aac0dda9a11e142c82344b5720e0
khusmuddin <khusmuddin.ansari@gmail.com>
2022-06-25 13:55:44 -0400

    array and obj

---
+++ learn-js-loop/index.js
@@ -0,0 +1,22 @@
+// Array
+let marks = [ 39,77,88,2,11]
+marks.push(3);
+marks.unshift(1)
+let array2 = [1,2,4,5,6,]
+
+marks = marks.concat(array2)
+console.log(marks);
+
+// object
+let obj = {
+    name: 'kd',
+    channel1: 'kdtech',
+    marks: [50,33,40,],
+    fruits: ['orange', 'mangoe', 'apple'],
+    address: {
+        road:'gali-num 7'
+    },
+}
+
+console.log(obj.address.road);
+
